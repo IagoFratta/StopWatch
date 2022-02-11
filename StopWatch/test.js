8d822b07ecfaf78735a4ebce4c2c1f4f253e365a
IagoFratta <90704093+IagoFratta@users.noreply.github.com>
2022-02-11 14:54:38 -0300

    Add files via upload

---
+++ StopWatch/test.js
@@ -0,0 +1,55 @@
+let hh = 0
+let mm = 0
+let ss = 0
+let iniciar = false
+let cron
+
+function start(){
+    if(!iniciar){
+       cron = setInterval(() => {
+        timer()
+    },1000)
+        console.log(cron)
+    }
+   
+}
+
+function stop(){
+    iniciar = false
+    clearInterval(cron)
+
+}
+
+function reset(){
+    
+    clearInterval(cron)
+    hh = 0
+    mm = 0
+    ss = 0
+
+    document.getElementById("cont").innerHTML = "00:00:00"
+}
+
+function timer(){
+    iniciar = true
+    ss++
+
+    if(ss == 60){
+        ss = 0
+        mm++
+
+    }
+    if(mm == 60){
+        mm = 0
+        hh++ 
+
+    }
+    var format = 
+    (hh < 10 ? "0" + hh : hh) +
+    ":" +
+    (mm < 10 ? "0" + mm : mm)+
+    ":"+
+    (ss < 10 ? "0" + ss : ss)
+
+    document.getElementById("cont").innerHTML = format
+}
